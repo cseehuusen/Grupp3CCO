7784d710fc64d35472807e68a16e868a435ca310
Mellowtide-CH <stavlama@hotmail.com>
2020-10-26 13:46:38 +0100

    Test update

---
+++ Assets/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, y, 0);
+        if (movement.sqrMagnitude > 1)
+            movement = movement.normalized;
+
+
+
+        rb2d.velocity = movement * speed;
+    }
+}
+
