a785be924bb148bd9a4acbf93b6e260af0bf6f76
Mellowtide-CH <stavlama@hotmail.com>
2020-10-27 17:09:20 +0100

    enemy prefab

---
+++ Assets/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+     anim.SetBool("meleeRange", true);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+     anim.SetBool("meleeRange", false);
+    }
+
+}
