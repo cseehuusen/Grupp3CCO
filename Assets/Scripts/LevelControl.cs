c413f524efa2ddbeec919488a5a27511a9a8007b
cseehuusen <cseehuusen@gmail.com>
2020-10-28 16:15:27 +0100

    Update: Fixed jump scene, heavies

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+
+    public string levelName;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            //Loading level with build levelName
+            SceneManager.LoadScene(levelName);
+
+            //Restart level
+            //SceneManager.LoaddScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
