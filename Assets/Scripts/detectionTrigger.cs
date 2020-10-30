4fa7085c7f41509c58631e8ba1240fb97fca7af6
cseehuusen <cseehuusen@gmail.com>
2020-10-30 10:14:53 +0100

    Fixed_BulletDetection_ Teleport_Problem

--- Assets/Scripts/detectionTrigger.cs
+++ Assets/Scripts/detectionTrigger.cs
@@ -6,10 +6,12 @@ public class detectionTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" || collision.tag ==  "PlayerBullet")
         {
             Enemy detectionVar = GetComponentInParent<Enemy>();
             detectionVar.detectPC = true;
+
+
         }
 
 
--- Assets/Scripts/detectionTrigger2.cs
+++ Assets/Scripts/detectionTrigger2.cs
@@ -6,7 +6,7 @@ public class detectionTrigger2 : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" || collision.tag == "PlayerBullet")
         {
             Enemy2 detectionVar = GetComponentInParent<Enemy2>();
             detectionVar.detectPC = true;
