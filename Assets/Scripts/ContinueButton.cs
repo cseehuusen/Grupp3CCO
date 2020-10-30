9abd6f4cd7b53bafb358543a7a46552bc284c83c
cseehuusen <cseehuusen@gmail.com>
2020-10-30 11:05:50 +0100

    Fixed_Victory_ContinueToMenu_ButtonAdded

--- Assets/Scripts/ContinueButton.cs
+++ Assets/Scripts/ContinueButton.cs
@@ -9,4 +9,9 @@ public class ContinueButton : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
